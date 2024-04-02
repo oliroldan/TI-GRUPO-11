0d86086b331dced9d6b57057ea2589aa2f418ff6
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2024-04-02 19:58:43 -0300

    avances

--- TI/app.js
+++ TI/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const profileRouter = require('./routes/profile')
+const { profile } = require('console');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/index.ejs', indexRouter);
+app.use('/profile.ejs', profileRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- TI/routes/index.js
+++ TI/routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// get todos los productos
+router.get('/index', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
