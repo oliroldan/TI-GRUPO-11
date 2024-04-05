4263aff1124853214dfce576d6674ffe11c99227
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2024-04-05 19:27:26 -0300

    ultimo form sin terminar

---
+++ TI/db/datos.js
@@ -0,0 +1,24 @@
+const datos = {
+    usuario: {
+        usuario: 'usuario?',
+        constrasena: 'contra?',
+        recordarme: 'si/no?',
+    },
+    
+    productos: [
+        {
+            imagen: 'href?',
+            nombreProducto: 'nombre?',
+            descripcion: 'texto?',
+            comentarios: [
+                {
+                nombreUsuario: 'usuario?',
+                texto: 'texto?',
+                imagenPerfil: 'href?'
+                }
+            ]
+        }
+    ]
+};
+
+module.exports = datos
