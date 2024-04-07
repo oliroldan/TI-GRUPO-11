008dde502c128b147873bcabf5a26bf255d86047
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2024-04-07 20:10:49 -0300

    database

--- TI/db/datos.js
+++ TI/db/datos.js
@@ -1,20 +1,128 @@
 const datos = {
     usuario: {
-        usuario: 'usuario?',
-        constrasena: 'contra?',
-        recordarme: 'si/no?',
+        usuario: 'zdbor1234',
+        constrasena: 'Hola1234',
+        recordarme: 'si',
     },
     
     productos: [
         {
-            imagen: 'href?',
-            nombreProducto: 'nombre?',
-            descripcion: 'texto?',
+            imagen: './public/productos/baseFitme.jpg',
+            nombreProducto: 'Base Fitme',
+            descripcion: 'La base Fit Me® Matte + Poreless matifica la piel y refina los poros para un acabado naturalmente hermoso e impecable.',
             comentarios: [
                 {
-                nombreUsuario: 'usuario?',
-                texto: 'texto?',
-                imagenPerfil: 'href?'
+                nombreUsuario: 'mora001',
+                texto: 'Buen producto! Me encanto',
+                imagenPerfil: './public/fotosPerfil/foto1.aviv'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/blushMac.jpg',
+            nombreProducto: 'Blush Mac',
+            descripcion: 'Mac Cosmetics Powder Blush Rubor Polvo Varios Tonos 06g. Se aplica de manera uniforme, se adhiere a la piel para lograr una aplicación de color con aspecto natural que permanece durante todo el día.',
+            comentarios: [
+                {
+                nombreUsuario: 'luz002',
+                texto: 'No me parece que valga lo que sale, igualmente es un buen producto.',
+                imagenPerfil: './public/fotosPerfil/foto2.jpeg'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/bronzer.jpg',
+            nombreProducto: 'Bronzer Rare Beauty',
+            descripcion: 'Los Bronzer Sticks de Rare Beauty son una adición innovadora y versátil para el contorneado, diseñados para ofrecer una aplicación sencilla y resultados naturales.',
+            comentarios: [
+                {
+                nombreUsuario: 'sol003',
+                texto: 'Impecable producto! Y muy buen envase.',
+                imagenPerfil: './public/fotosPerfil/foto3.aviv'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/lipglossDior.jpg',
+            nombreProducto: 'Lipgloss Dior',
+            descripcion: 'Descubre el brillo y el glamour en cada sonrisa con el lip gloss Dior, una verdadera obra maestra de la belleza que elevará tus labios a un proximo nivel',
+            comentarios: [
+                {
+                nombreUsuario: 'maia004',
+                texto: 'Tan bueno que se me termino al toque! Por lo que sale me tendria que haber controlado',
+                imagenPerfil: './public/fotosPerfil/foto4.webp'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/mascara.jpg',
+            nombreProducto: 'Mascara Maybelline',
+            descripcion: 'Es un cosmético totalmente a prueba de agua, formulado con extracto de bambú y fibras para ayudar a cuidar tus pestañas. Además, no deja grumos, ni manchas.',
+            comentarios: [
+                {
+                nombreUsuario: 'mariana005',
+                texto: 'Buenisima calidad! Y el waterproof es real.',
+                imagenPerfil: './public/fotosPerfil/foto5.png'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/bronzerChanel.jpg',
+            nombreProducto: 'Bronzer Chanel',
+            descripcion: 'Esta crema gel bronceadora única, ligera y fácil de difuminar ofrece un acabado delicado y aterciopelado para un resplandor bronceado natural.',
+            comentarios: [
+                {
+                nombreUsuario: 'kari006',
+                texto: 'Fanatica del producto! Lo volveria a comprar.',
+                imagenPerfil: './public/fotosPerfil/foto6.webp'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/base2.jpg',
+            nombreProducto: 'Base Essence',
+            descripcion: 'Base de maquillaje en polvo, resistente al agua y matificante, con gran cobertura y 16 horas de duración.',
+            comentarios: [
+                {
+                nombreUsuario: 'pili007',
+                texto: 'Muy buena base! La mejor de todas las que probe.',
+                imagenPerfil: './public/fotosPerfil/foto7.png'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/highlighterRare.jpg',
+            nombreProducto: 'Highlighter Rare Beauty',
+            descripcion: 'Un iluminador en polvo innovador que le da a la piel un brillo instantáneo como el del vidrio para un brillo fácil y de apariencia natural que dura.',
+            comentarios: [
+                {
+                nombreUsuario: 'trini008',
+                texto: 'Increible producto! Selena no falla',
+                imagenPerfil: './public/fotosPerfil/foto8.png'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/eyeshadow.jpg',
+            nombreProducto: 'Eyeshadow Charlotte Tilbury',
+            descripcion: 'Paleta de sombras de ojos con tonos nude rosa, rosa intenso y bronce melocotón para lograr unos ojos Pillow Talk DESLUMBRANTES Y SEDUCTORES. ',
+            comentarios: [
+                {
+                nombreUsuario: 'connie009',
+                texto: 'No soy fanatica del prducto.',
+                imagenPerfil: './public/fotosPerfil/foto9.jpeg'
+                }
+            ]
+        },
+        {
+            imagen: './public/productos/skincare2.jpg',
+            nombreProducto: 'Skincare Todomoda',
+            descripcion: 'Tónico iluminador con ácido hialurónico y extracto de sandía, arándanos, naranja y limón. Reabastece la hidratación para nutrir y suavizar la piel.',
+            comentarios: [
+                {
+                nombreUsuario: 'rena010',
+                texto: 'Cuestionable calidad, pero funciona y precio accesible',
+                imagenPerfil: './public/fotosPerfil/foto10.webp'
                 }
             ]
         }
