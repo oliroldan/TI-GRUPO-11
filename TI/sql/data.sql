fd2d7e2c291eec4b3b37bae2d0a491b344f6ee21
oliroldan <oroldan@udesa.edu.ar>
2024-04-08 16:39:25 -0300

    Agregue la tabla de productos

--- TI/sql/data.sql
+++ TI/sql/data.sql
@@ -15,3 +15,15 @@ CREATE TABLE usuarios (
     /*los FK*/
     /* FOREIGN KEY (idGenero) REFERENCES generos(id) */
 );
+
+CREATE TABLE productos (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	fotoProducto VARCHAR(250),
+    nombre VARCHAR(250),
+    descripcion VARCHAR(250),
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt           TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
+    /los FK/
+    /* FOREIGN KEY (id) REFERENCES usuarios(id) */
+);
