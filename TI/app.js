df0d269201e8bf254ae1e516c350850ed7d458f3
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2024-04-08 19:52:07 -0300

    volvio a aparecer el logo

--- TI/app.js
+++ TI/app.js
@@ -22,17 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
 app.use('/users', usersRouter);
 
-// app.use('/profile', usersRouter);
-// app.use('/login', usersRouter);
+//app.use('/profile', usersRouter);
+//app.use('/login', usersRouter);
 //app.use('/register', usersRouter);
 //app.use('/profile-edit', usersRouter);
 
 app.use('/product', productRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
