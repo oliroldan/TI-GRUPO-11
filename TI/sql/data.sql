3e12ccca97b9640dfd67a7a7a589881a6fb7c224
Zoe Del Blanco <zoedelblanco@Zoes-MacBook-Air.local>
2024-04-08 16:34:38 -0300

    tabla usuario

---
+++ TI/sql/data.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA datos;
+
+USE datos; 
+
+CREATE TABLE usuarios (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	mail VARCHAR(250),
+    contra VARCHAR(250),
+    fecha DATE,
+    dni INT,
+    fotoPerfil VARCHAR(250),
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt           TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
+    /*los FK*/
+    /* FOREIGN KEY (idGenero) REFERENCES generos(id) */
+);
